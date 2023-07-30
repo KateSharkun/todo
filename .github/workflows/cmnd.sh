f0a2cda08604623e956950b9f721599b778def33
KateSharkun <90849014+KateSharkun@users.noreply.github.com>
2023-07-30 19:10:03 +0200

    Delete cmnd.sh

--- .github/workflows/cmnd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting *sh commands"
-cd /home
-current_date_time=`date +"%Y-%m-%d %T"`
-git pull https://github.com/KateSharkun/Todo.git
-git commit -m 'deploy on $current_date_time'
-git push
-systemctl restart todo
-systemctl status todo
